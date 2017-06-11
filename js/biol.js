e8ae1e27c34aab8c50a471af87a8a1c933ee94a8
yunelumbot <yui.aragaki.fansite@gmail.com>
2017-06-11 07:39:36 +0000

    ボタン押下処理修正中

--- js/biol.js
+++ js/biol.js
@@ -9,10 +9,10 @@ function clickNode(divId) {
 
 function clickNode2(divId) {
     var request = new XMLHttpRequest();
-    request.open("GET", "/web/json/biol/apg4.json", true);
+    request.open("GET", "/web/json/biol/apg4.json", false);
     request.send(null);
     if (request.status == 200) {
-        var apg4;
+        var apg4 = "";
         eval("apg4 = " + request.responseText);
         var contents;
         for (var i = 0; i < apg4[divId].length; i++) {
