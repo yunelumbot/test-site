6322a1fca99fccf29e21531dd721ac8423cada8f
yunelumbot <yui.aragaki.fansite@gmail.com>
2017-06-18 07:13:32 +0000

    クリック処理検討中

--- js/biol.js
+++ js/biol.js
@@ -12,8 +12,7 @@ function clickNode2(divId) {
     request.open("GET", "/web/json/biol/apg4.json", false);
     request.send(null);
     if (request.status == 200) {
-        var apg4;
-        eval("apg4 = " + request.responseText);
+        var apg4 = JSON.parse(request.responseText);
         var contents = "";
         for (var i = 0; i < apg4[divId].length; i++) {
             contents += apg4[divId][i] + " ";
